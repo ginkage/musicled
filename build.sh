48fa1cb2166473b38bf628fea19c5d9cd0172fbc
Ivan Podogov <ginkage@yandex.ru>
2018-11-11 10:28:14 +0000

    Rename source files

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p .deps
+g++ -std=c++11 -Wall -Wextra -Wno-unused-result -Wno-maybe-uninitialized -D_GNU_SOURCE -D_POSIX_SOURCE -D _POSIX_C_SOURCE=200809L -g -O2 -MT musicled.o -MD -MP -MF .deps/musicled.Tpo -c -o musicled.o ./musicled.cpp
+g++ -std=c++11 -Wall -Wextra -Wno-unused-result -Wno-maybe-uninitialized -g -O2 -Wl,-rpath /usr/local/lib -o musicled musicled.o  -L/usr/local/lib -lpthread -lasound -lm -lfftw3 -ltinfo -lX11
