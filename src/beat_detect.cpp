c7e37ca621071716af04d42c0879c9b9418191e0
Ivan Podogov <ginkage@yandex.ru>
2020-09-04 14:07:59 +0100

    Split common part into a separate class

---
+++ src/beat_detect.cpp
@@ -0,0 +1,45 @@
+#include "beat_detect.h"
+
+#include <iostream>
+#include <numeric>
+
+BeatDetect::BeatDetect(
+    GlobalState* state, CircularBuffer<Sample>* buf, std::shared_ptr<ThreadSync> ts)
+    : global(state)
+    , samples(buf)
+    , values(1024)
+    , sync(ts)
+{
+}
+
+void BeatDetect::start_thread()
+{
+    thread = std::thread([this] { loop(); });
+}
+
+void BeatDetect::join_thread() { thread.join(); }
+
+void BeatDetect::loop()
+{
+    int64_t last_read = 0;
+    while (!global->terminate) {
+        sync->consume(
+            [&] {
+                // If we have enough data (or it's time to stop)
+                return global->terminate
+                    || (samples->get_latest() >= last_read + (int64_t)values.size());
+            },
+            [&] {
+                // Then read the oldest samples while we're still locked to avoid missing data
+                if (!global->terminate) {
+                    last_read = samples->read_at(last_read, values.data(), values.size());
+                }
+            },
+            [&] {
+                // And continue the analysis after unlocking the mutex
+                if (!global->terminate) {
+                    detect();
+                }
+            });
+    }
+}
--- src/beat_energy.cpp
+++ src/beat_energy.cpp
@@ -5,46 +5,12 @@
 
 BeatEnergy::BeatEnergy(
     GlobalState* state, CircularBuffer<Sample>* buf, std::shared_ptr<ThreadSync> ts)
-    : global(state)
-    , samples(buf)
+    : BeatDetect(state, buf, ts)
     , energy(43)
-    , values(1024)
-    , sync(ts)
 {
 }
 
-void BeatEnergy::start_thread()
-{
-    thread = std::thread([this] { beat_detect(); });
-}
-
-void BeatEnergy::join_thread() { thread.join(); }
-
-void BeatEnergy::beat_detect()
-{
-    int64_t last_read = 0;
-    while (!global->terminate) {
-        sync->consume(
-            [&] {
-                // If we have enough data (or it's time to stop)
-                return global->terminate || (samples->get_latest() >= last_read + values.size());
-            },
-            [&] {
-                // Then read the oldest samples while we're still locked to avoid missing data
-                if (!global->terminate) {
-                    last_read = samples->read_at(last_read, values.data(), values.size());
-                }
-            },
-            [&] {
-                // And continue the analysis after unlocking the mutex
-                if (!global->terminate) {
-                    compute_energy();
-                }
-            });
-    }
-}
-
-void BeatEnergy::compute_energy()
+void BeatEnergy::detect()
 {
     // Latest energy value
     double last_e = 0;
@@ -74,7 +40,8 @@ void BeatEnergy::compute_energy()
         // std::cout << "E = " << last_e << ", Avg = " << avg_e << ", V = " << variance << ", C = "
         // << c << std::endl << std::flush;
         if (last_e > c * avg_e) {
-            std::cout << "Boom! " << (1.5142857 - (last_e / avg_e)) / variance << std::endl << std::flush;
+            std::cout << "Boom! " << (1.5142857 - (last_e / avg_e)) / variance << std::endl
+                      << std::flush;
         }
     }
 }
