6a2513d7476f82b938326d5c1734233592f24aa9
Ivan Podogov <ginkage@yandex.ru>
2020-09-08 01:36:52 +0100

    Slightly faster wavelets

--- src/daubechies4.cpp
+++ src/daubechies4.cpp
@@ -25,17 +25,20 @@ decomposition Daubechies4::forward(std::vector<double>& data)
     int length = data.size();
     int half = length >> 1;
     int mask = length - 1;
-    std::vector<double> energy = std::vector<double>(half, 0);
-    std::vector<double> detail = std::vector<double>(half, 0);
+    std::vector<double> energy = std::vector<double>(half);
+    std::vector<double> detail = std::vector<double>(half);
 
     for (int i = 0; i < half; ++i) {
+        double e = 0, d = 0;
         for (int j = 0; j < 4; ++j) {
-            int k = ((i << 1) + j) & mask;
+            double v = data[((i << 1) + j) & mask];
             // low pass filter for the energy (approximation)
-            energy[i] += data[k] * scalingDecom[j];
+            e += v * scalingDecom[j];
             // high pass filter for the details
-            detail[i] += data[k] * waveletDecom[j];
+            d += v * waveletDecom[j];
         }
+        energy[i] = e;
+        detail[i] = d;
     }
 
     return { energy, detail };
